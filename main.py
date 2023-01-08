5b8e7696e9a564df834927335777c1850b6d34a6
APPALERMO <86126373+APPALERMO@users.noreply.github.com>
2023-01-08 20:37:06 +0100

    Update main.py

--- main.py
+++ main.py
@@ -5,4 +5,4 @@ print("Your password is:", password, end="\n")
 
 PasswordGenerator.PasswordAmount(password, 10)
 
-PasswordGenerator.createWordList(password, "Test_WordList.txt", 20000, False)  # if in the false place we put "True" it tells us how long it takes
+PasswordGenerator.createWordList(password, "Test_WordList.txt", 20000)
