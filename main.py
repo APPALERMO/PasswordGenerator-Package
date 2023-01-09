844ad91a80e03c23a8bb97c531fb966660c92172
APPALERMO <86126373+APPALERMO@users.noreply.github.com>
2023-01-09 17:24:25 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from PasswordGenerator import PasswordGenerator
+from PasswordGenerator.gui import PasswordGUI
 
 password = PasswordGenerator(100, 3)
 print("Your password is:", password, end="\n")
 
 PasswordGenerator.PasswordAmount(password, 10)
-
 PasswordGenerator.createWordList(password, "Test_WordList.txt", 20000)
+
+print("GUI Avviata")
+PasswordGUI().mainloop()
+print("GUI Chiusa")
